129ca8cd0ffa369bc56759d46fed17af6095f136
austindoeswork <wilsonaustin17@gmail.com>
2016-05-12 10:33:42 -0400

    added scaler

--- hello.go
+++ hello.go
@@ -3,6 +3,26 @@ package main
 import ("fmt")
 
 
+func fscale(total int, needed int, floor int, ROOMS map[int]Room) {
+
+	ftotal := total - (floor*len(ROOMS))
+	fneeded := needed - (floor*len(ROOMS))
+	var fratio float64 = (float64(fneeded)/float64(ftotal))
+
+
+	// var ratio float64 = (float64(needed)/float64(total))
+	var finalTotal float64 = 0
+	for name,room := range ROOMS {
+		fmt.Print(name)
+		fmt.Print(": ")
+		temp := (float64(room.Dowry)-float64(floor))*fratio + float64(floor)
+		finalTotal = finalTotal + temp
+		fmt.Println(temp)
+	}
+	fmt.Println(finalTotal)
+}
+
+
 func main(){
 	//parse first and generate a ROOMS map and TENANTS map
 	ROOMS,TENANTS := Parse("csvs/")
@@ -34,6 +54,9 @@ func main(){
 		totalOffer += room.Dowry
 	}
 	fmt.Print("\nTOTAL RENT OFFERED: ")
+
 		fmt.Println(totalOffer)
 
+	fscale(totalOffer, 3420, 300, ROOMS)
+	fscale(totalOffer, 3420, 0, ROOMS)
 }
